5b52c3d85899ebe6768d37552b80e3da6325d351
dikshant2210 <dikshant2210@gmail.com>
2017-09-12 23:28:17 +0530

    added functions for training the network

--- train.py
+++ train.py
@@ -1,6 +1,17 @@
-from utils import normalize_string, filter_pairs
+from utils import normalize_string, filter_pairs, MAX_LENGTH, time_since, show_plot
 from preprocessing import Lang
+from preprocessing import EOS_token, SOS_token
+from torch.autograd import Variable
+from models.decoder import DecoderRNN
+from models.encoder import EncoderRNN
+import torch.nn as nn
+import torch.optim as optim
+import torch
+import time
 import random
+
+
+use_cuda = torch.cuda.is_available()
 
 
 def read_langs(lang1, lang2, reverse=False):
@@ -36,3 +47,103 @@ def prepare_data(lang1, lang2, reverse=False):
 
 input_lang, output_lang, pairs = prepare_data('eng', 'fra', True)
 print(random.choice(pairs))
+
+
+def indexes_from_sentence(lang, sentence):
+    return [lang.word2index[word] for word in sentence.split(' ')]
+
+
+def variable_from_sentence(lang, sentence):
+    indexes = indexes_from_sentence(lang, sentence)
+    indexes.append(EOS_token)
+    result = Variable(torch.LongTensor(indexes).view(-1, 1))
+    if use_cuda:
+        return result.cuda()
+    else:
+        return result
+
+
+def variable_from_pair(pair):
+    input_variable = variable_from_sentence(input_lang, pair[0])
+    output_variable = variable_from_sentence(output_lang, pair[1])
+    return (input_variable, output_variable)
+
+
+def train(input_variable, target_variable, encoder, decoder,
+          encoder_optimizer, decoder_optimizer, criterion, max_length=MAX_LENGTH):
+    encoder_hidden = encoder.initHidden()
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+    input_length = input_variable.size()[0]
+    target_length = target_variable.size()[0]
+
+    loss = 0
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(input_variable[ei], encoder_hidden)
+
+    encoder_hidden = encoder_hidden.cuda() if use_cuda else encoder_hidden
+    decoder_input = Variable(torch.LongTensor([[SOS_token]]))
+    decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+    decoder_hidden = encoder_hidden
+
+    for di in range(target_length):
+        decoder_output, decoder_hidden = decoder(decoder_input, decoder_hidden)
+        topv, topi = decoder_output.data.topk(1)
+        ni = topi[0][0]
+        decoder_input = Variable(torch.LongTensor([[ni]]))
+        decoder_input = decoder_input.cuda() if use_cuda else decoder_input
+
+        loss += criterion(decoder_output, target_variable[di])
+        if ni == EOS_token:
+            break
+    loss.backward()
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.data[0] / target_length
+
+
+def train_iters(encoder, decoder, n_iters, print_every=1000, plot_every=100, learning_rate=0.01):
+    start = time.time()
+    plot_losses = []
+    print_loss_total = 0
+    plot_loss_total = 0
+
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_pairs = [variable_from_pair(random.choice(pairs)) for i in range(n_iters)]
+
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, n_iters + 1):
+        training_pair = training_pairs[iter - 1]
+        input_variable = training_pair[0]
+        target_variable = training_pair[1]
+        loss = train(input_variable, target_variable, encoder, decoder,
+                     encoder_optimizer, decoder_optimizer, criterion)
+        print_loss_total += loss
+        plot_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('%s (%d %d%%) %.4f' % (time_since(start, iter / n_iters),
+                                         iter, iter / n_iters * 100, print_loss_avg))
+
+        if iter % plot_every == 0:
+            plot_loss_avg = plot_loss_total / plot_every
+            plot_losses.append(plot_loss_avg)
+            plot_loss_total = 0
+
+    show_plot(plot_losses)
+
+
+hidden_size = 256
+encoder1 = EncoderRNN(input_lang.n_words, hidden_size)
+decoder1 = DecoderRNN(hidden_size, output_lang.n_words)
+
+if use_cuda:
+    encoder1 = encoder1.cuda()
+    decoder1 = decoder1.cuda()
+
+train_iters(encoder1, decoder1, 75000, print_every=5000)
