9ab893a73a0d654cbd6cdf03386f97855af7f30b
dikshant2210 <dikshant2210@gmail.com>
2017-09-12 14:52:44 +0530

    utility functions for cleaning and processing data

---
+++ utils.py
@@ -0,0 +1,37 @@
+import unicodedata
+import re
+
+
+MAX_LENGTH = 10
+eng_prefixes = (
+    "i am", " i m ",
+    "he is", "he s ",
+    "she is", "she s",
+    "you are", "you re ",
+    "we are", "we re ",
+    "they are", "they re "
+)
+
+
+def unicode_to_ascii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+    )
+
+
+def normalize_string(s):
+    s = unicode_to_ascii(s.lower().strip())
+    s = re.sub(r"([.!?])", r" \1", s)
+    s = re.sub(r"[^a-zA-z.!?]", r" ", s)
+    return s
+
+
+def filter_pair(p):
+    return len(p[0].split(' ')) < MAX_LENGTH and \
+        len(p[1].split(' ')) < MAX_LENGTH and \
+        p[1].startswith(eng_prefixes)
+
+
+def filter_pairs(pairs):
+    return [pair for pair in pairs if filter_pair(pair)]
