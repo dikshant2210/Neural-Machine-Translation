2399e68d4fd9101f4b65933be99139b254f64028
dikshant2210 <dikshant2210@gmail.com>
2017-09-14 17:14:07 +0530

    first commit for attention based decoder

---
+++ models/attn_decoder.py
@@ -0,0 +1,24 @@
+from torch.autograd import Variable
+from utils import MAX_LENGTH
+import torch.nn as nn
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size, n_layers, dropout_p=0.1, max_length=MAX_LENGTH):
+        super(AttnDecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.n_layers = n_layers
+        self.dropout_p = dropout_p
+        self.max_length = max_length
+
+        self.embedding = nn.Embedding(self.output_size, self.hidden_size)
+        self.attn = nn.Linear(self.hidden_size * 2, self.max_length)
+        self.attn_combine = nn.Linear(self.hidden_size * 2, self.hidden_size)
+        self.dropout = nn.Dropout(self.dropout_p)
+        self.gru = nn.GRU(self.hidden_size, self.hidden_size)
+        self.out = nn.Linear(self.hidden_size, self.output_size)
+
+    def forward(self, input, hidden, encoder_output, encoder_outputs):
+        embedded = self.embedding(input).view(1, 1, -1)
+        embedded = self.dropout(embedded)
